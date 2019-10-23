a1b2e3614ad15b68865eb27c55ff4791675890a7
potetoo521@gmail.com <potetoo521@gmail.com>
2019-10-23 18:19:20 +0900

    主人公の基本操作　ダッシュ。ジャンプ。自由落下。慣性。

---
+++ WAVE/WAVE/SceneMain.cpp
@@ -0,0 +1,45 @@
+//STLデバッグ機能をOFFにする
+#define _SECER_SCl (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawTexture.h"
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include "SceneMain.h"
+#include "GameHead.h"
+
+//コンストラクタ
+CSceneMain::CSceneMain()
+{
+
+}
+
+//デストラクタ
+CSceneMain::~CSceneMain()
+{
+
+}
+
+//初期化メソッド
+void CSceneMain::InitScene()
+{
+	//グラフィック読み込み
+	Draw::LoadImageW(L"image1.png",0,TEX_SIZE_512);
+
+	//主人公オブジェクト作成
+	CObjHero* obj = new CObjHero();
+	Objs::InsertObj(obj, OBJ_HERO, 10);
+
+}
+
+//実行中メソッド
+ void CSceneMain::Scene()
+{
+
+}
+
+
