95135c5893c783d8bac8c55c53d41a0106ef3578
翔稀 井上 <h1201919033@hiratagakuen.onmicrosoft.com>
2019-12-19 09:29:42 +0900

    弾丸の引数を変えました

--- WAVE/WAVE/GameHead.h
+++ WAVE/WAVE/GameHead.h
@@ -148,5 +148,5 @@ extern float kazu;
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START CSceneMain
+#define SET_GAME_START CSceneBlock2
 //-----------------------------------------------
